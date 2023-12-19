05650a2318cdbf9e01f27ab9abe1920461b1e2f9
Spongebobsleigh <shimoshun04@gmail.com>
2023-12-20 00:18:57 +0900

    update

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // 前方移動力.
+    [SerializeField] float movePower = 20000f;
+    // リジッドボディ.
+    Rigidbody rigid = null;
+
     void Start()
     {
-        
+        if (rigid == null) rigid = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void FixedUpdate()
+    {
+        MoveUpdate();
+    }
+
+    // ------------------------------------------------------------
+    /// <summary>
+    /// 移動処理.
+    /// </summary>
+    // ------------------------------------------------------------
+    void MoveUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space) == true)
+        {
+            rigid.AddForce(transform.forward * movePower, ForceMode.Force);
+        }
     }
 }
